d7ca6f1bf06b3e09b324cb1e3c7ec9d741dde4ad
cefey <staffa261@gmail.com>
2021-02-21 19:51:03 +0300

    Что такое тестирование [#476571]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -3,19 +3,21 @@ package ru.job4j;
 public class Converter {
 
     public static int rubleToEuro(int value) {
-        int rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static int rubleToDollar(int value) {
-        int rsl = value / 60;/* формула перевода рублей в доллары. */
-        return rsl;
+        return value / 60;
     }
 
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
+        int expected = 2;
+        boolean passed = expected == euro;
+        System.out.println("140 rubles are 2 euro. Test result: " + passed);
         int dollar = Converter.rubleToDollar(180);
-        System.out.println("180 rubles are " + dollar + " dollar.");
+        expected = 3;
+        passed = expected == dollar;
+        System.out.println("180 rubles are 3 dollars. Test result: " + passed);
     }
 }
--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -8,7 +8,8 @@ public class SqArea {
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(4, 1);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+        char c = 45000;
+        float i = c;
+        System.out.println(i);
     }
 }
