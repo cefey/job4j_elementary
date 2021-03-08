efe76eeaf35b93521729ce1c711643bd8d8a1ee3
cefey <staffa261@gmail.com>
2021-03-08 10:16:19 +0300

    Отладка программы в IDEA [#476485] (Исправление)

--- src/ru/job4j/loop/Factorial.java
+++ src/ru/job4j/loop/Factorial.java
@@ -2,11 +2,11 @@ package ru.job4j.loop;
 
 public class Factorial {
     public static int calc(int n) {
-        int result = 0;
-        if ((n == 0) || (n == 1)) {
-            return 1;
-        }
-        result = 1;
+//        int result = 0;
+//        if ((n == 0) || (n == 1)) {
+//            return 1;
+//        }
+        int result = 1;
         for (int i = 2; i <= n; i++) {
           result = result * i;
         }
