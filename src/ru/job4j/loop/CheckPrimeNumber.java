029e95669fb6d152c9d6d94227323ed8927682ce
cefey <staffa261@gmail.com>
2021-03-23 23:00:13 +0300

    Классический поиск перебором. [#476505]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,9 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+       boolean prime = true;
+//        for (int i=0; i < number)
+       return prime;
+    }
+}
