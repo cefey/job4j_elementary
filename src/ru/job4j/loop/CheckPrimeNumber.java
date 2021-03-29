921064070d76d42afae03cf7191b47ac268886a4
cefey <staffa261@gmail.com>
2021-03-29 15:55:39 +0300

    Простое число [#476599]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,7 +3,15 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
        boolean prime = true;
-//        for (int i=0; i < number)
+      if (number == 1) {  //принудительно сделаем проверку на ввод 1
+          return false;
+      }
+        for (int i = 2; i < number; i++) {
+           if (number % i == 0) {
+               prime = false;
+               break;
+           }
+        }
        return prime;
     }
 }
