707edd74b991ee1022a793d15b4eb0368e2caaaa
cefey <staffa261@gmail.com>
2021-04-02 20:43:46 +0300

    Ипотека [#476598]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        double debt = amount; //долг на текущий год
+        while (debt > 0) {
+                debt = (debt + ((debt * percent) / 100)) - salary;
+                year++;
+        }
+        return year;
+
+    }
+
+}
