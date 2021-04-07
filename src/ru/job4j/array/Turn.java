3c6b1709a03291217481323e52e2a753c161eea5
cefey <staffa261@gmail.com>
2021-04-07 21:32:27 +0300

    Перевернуть массив. [#476523]

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -13,20 +13,4 @@ public class Turn {
         }
         return array;
     }
-
-    public static void main(String[] args) {
-        int[] arr = {90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
-        int[] arr2 = new int[10];
-        System.out.println("Ввели массив ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        arr2 = back(arr);
-        System.out.println();
-        System.out.println("На выходе получили ");
-        for (int i = 0; i < arr2.length; i++) {
-            System.out.print(arr2[i] + " ");
-        }
-
-    }
 }
