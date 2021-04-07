dda5abaa3a3544e96cfa9462b266e8d1004ec159
cefey <staffa261@gmail.com>
2021-04-07 21:37:40 +0300

    Перевернуть массив. [#476523]

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -5,7 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 public class Turn {
     public static int[] back(int[] array) {
         int temp;
-        int n = array.length - 1;
+        int n = array.length - 1;  //номер последнего элемента в массиве
         for (int i = 0; i < array.length / 2; i++) {
             temp = array[i];
             array[i] = array[n - i];
