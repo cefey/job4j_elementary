d362e133c3289b9e1254151c465bf44872d70718
cefey <staffa261@gmail.com>
2021-05-10 10:08:12 +0300

    Math.abs [#476498] (Исправление)

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -1,13 +1,10 @@
 package ru.job4j.condition;
 
 public class ChessBoard {
-    @SuppressWarnings("checkstyle:RightCurly")
     public static int way(int x1, int y1, int x2, int y2) {
         if (x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0 || x1 > 7 || x2 > 7 || y1 > 7  || y2 > 7) {
             return 0;
         }
-        return (Math.abs(x2 - x1) == Math.abs(y2 - y1)) ? Math.abs(x2 - x1) : 0;
-
+            return (Math.abs(x2 - x1) == Math.abs(y2 - y1)) ? Math.abs(x2 - x1) : 0;
         }
-
-    }
+}
