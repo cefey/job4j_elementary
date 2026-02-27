f246e09477e2ef5e90dd9980c65cc68dbff99891
cefey <staffa261@gmail.com>
2026-02-27 21:48:33 +0300

    4.1. Разбор первой программы. [#159033 #109454]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+   public static void main(String[] args) {
+    System.out.println("Vyacheslav Golovlev");
+    System.out.println("21.07.1985");
+
+   }
+}
