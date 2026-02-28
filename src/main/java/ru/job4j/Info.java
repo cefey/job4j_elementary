ca4cfcfe06de1b9ad619801b854ab40f15bd7bd0
cefey <staffa261@gmail.com>
2026-02-28 12:03:18 +0300

    6. Checkstyle [#4123 #109450]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class info {
-    public static void main(String[] args){
+public class Info {
+    public static void main(String[] args) {
         System.out.println("27.02.2026");
     }
 }
