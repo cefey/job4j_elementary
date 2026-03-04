ee64103ef0f5119451ec57a322f77ef5745979c5
cefey <staffa261@gmail.com>
2026-03-04 21:04:52 +0300

    4.1. Аргументы [#295521 #109444]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -6,4 +6,22 @@ public class StudentInfo {
     System.out.println("21.07.1985");
 
    }
+
+    public static class Calculator {
+        public static void main(String[] args) {
+            int one = 1;
+            int two = 2;
+            int onePlusTwo = one + two;
+            System.out.println(onePlusTwo);
+            int six = 6;
+            int four = 4;
+            int five = 5;
+            int sixDivTwo = six / two;
+            int fiveMinusTwo = five - two;
+            int fourTimeTwo = four * two;
+            System.out.println(sixDivTwo);
+            System.out.println(fiveMinusTwo);
+            System.out.println(fourTimeTwo);
+        }
+    }
 }
